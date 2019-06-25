71da738be62eb4d8324401ecf6c1249f0223099c
cwgaldino <carlosw.galdino@gmail.com>
2019-06-25 13:53:25 -0300

    derivative data update.

--- data/comparisson.py
+++ data/comparisson.py
@@ -13,8 +13,17 @@ import matplotlib.pyplot as plt
 import sys
 sys.path.append("/home/galdino/github/myModules")
 import filemanip as file
+from arraymanip import increasing_monotonicity
 
 %matplotlib qt5
+
+# %% Ordering data
+data = np.loadtxt(str('Medida_Z_Geral.csv'), skiprows=1)
+x = np.linspace(-35, 30, 1000)
+x_data, y_data = increasing_monotonicity(data[:, 0]-35, data[:,1])
+temp = {'z(mm)':x_data, 'Bz(G)':y_data}
+file.saveDataDict(temp, '.', 'medida_z_ordered.dat')
+
 
 # %% Bz
 plt.figure()
@@ -37,6 +46,26 @@ plt.plot(-data[:, 0]*1000, data[:, 1], label='Perfect coil simulation')
 data = np.genfromtxt(str('z_der_simi_real.csv'), skip_header=4, delimiter=',')
 plt.plot(-data[:, 0]*1000, data[:, 1], label='Square-packing coil simulation')
 data = np.loadtxt(str('Medida_Z_Geral.csv'), skiprows=1)
+plt.plot(data[:-1, 0]-35, -np.diff(data[:, 1])/np.diff(data[:, 0]) * 10, marker='o', linewidth=0, label='Measured')
+plt.xlabel('z (mm)')
+plt.ylabel('dBz/dz (G/cm)')
+plt.legend()
+plt.savefig('dBz.svg')
+
+# %% interpolated data
+plt.figure()
+data = np.loadtxt(str('Medida_Z_Geral.csv'), skiprows=1)
+x = np.linspace(-35, 30, 1000)
+from arraymanip import increasing_monotonicity
+x_data, y_data = increasing_monotonicity(data[:, 0]-35, data[:,1])
+temp = {'z(mm)':x_data, 'Bz(G)':y_data}
+file.saveDataDict(temp, '.', 'medida_z_ordered.dat')
+y = np.interp(x, x_data, y_data)
+plt.plot(x_data, y_data, marker='o', linewidth=0, label='')
+plt.plot(x, y, linewidth=.5, label='')
+# %%
+plt.figure()
+plt.plot(data[:-1, 0]-35, -np.diff(data[:, 1])/np.diff(data[:, 0]) * 10, marker='o', linewidth=0, label='Measured')
 plt.plot(data[:-1, 0]-35, -np.diff(data[:, 1])/np.diff(data[:, 0]) * 10, marker='o', linewidth=0, label='Measured')
 plt.xlabel('z (mm)')
 plt.ylabel('dBz/dz (G/cm)')
